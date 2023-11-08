3b8007eefc424dee3bc0976d4a9f8cafe03d5ec7
eylon gilad <elon.gilad9@gmail.com>
2023-11-08 15:11:52 +0200

    setup the openCV

---
+++ mobylie/mobylie/Main.cpp
@@ -0,0 +1,17 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+using namespace cv;
+
+int main()
+{
+    cv::Mat img = cv::imread("C:\\Users\\test0\\Downloads\\idk\\idk.png");
+    namedWindow("First OpenCV Application", WINDOW_AUTOSIZE);
+    cv::imshow("First OpenCV Application", img);
+    cv::moveWindow("First OpenCV Application", 0, 45);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+    return 0;
+}
+
