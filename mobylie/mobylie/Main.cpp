73ca4208f674f624cb6a9e42d33ebd5d7e5c3c2f
eylon gilad <elon.gilad9@gmail.com>
2023-11-21 01:15:36 +0200

    idk

--- mobylie/mobylie/Main.cpp
+++ mobylie/mobylie/Main.cpp
@@ -4,14 +4,55 @@
 #include <iostream>
 using namespace cv;
 
-int main()
-{
-    cv::Mat img = cv::imread("C:\\Users\\test0\\Downloads\\idk\\idk.png");
-    namedWindow("First OpenCV Application", WINDOW_AUTOSIZE);
-    cv::imshow("First OpenCV Application", img);
-    cv::moveWindow("First OpenCV Application", 0, 45);
-    cv::waitKey(0);
-    cv::destroyAllWindows();
+//////////////////opencv includes////////////////////
+
+
+//////////////////opencv includes////////////////////
+
+//////////////////pybind includes////////////////////
+#include <pybind11/embed.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/operators.h>
+namespace py = pybind11;
+
+//////////////////pybind includes////////////////////
+
+int main() {
+    //std::string const VIDEO_NAME = "highway1.mp4";
+    //bool const LIVE = false;
+    //cv::namedWindow("test", cv::WINDOW_AUTOSIZE);
+    //cv::VideoCapture cap;
+    //vidLoader loader("videos/" + VIDEO_NAME);
+    ////vidLoader loader("");
+    //cv::Mat frame = loader.getNextFrame();
+    //while (!frame.empty())
+    //{
+
+    //    cv::imshow("test", frame);
+    //    if (cv::waitKey(1) >= 0)
+    //    {
+    //        break;
+    //    }
+    //    frame = loader.getNextFrame();
+    //}
+    //cv::destroyAllWindows();
+    try
+    {
+        // Create a scoped interpreter for the second Python environment
+        pybind11::scoped_interpreter interpreter2("/Scripts/python.exe");
+
+        // Import the Python module from the second environment
+        auto nul = py::module::import("detection_from_folder"); // import the sign module
+        auto multipl = nul.attr("main"); // get fanc
+        //multipl("C:/Users/test0/Downloads/idkk/input_images"); // use
+        multipl();
+        auto test = py::module::import("detect");//import the car(and poeple) module
+        auto testFanc = test.attr("run");//get fanc
+        testFanc();//use
+    }
+    catch (const py::error_already_set& e) {
+        std::cerr << "Python error: " << e.what() << std::endl;
+    }
     return 0;
 }
 
