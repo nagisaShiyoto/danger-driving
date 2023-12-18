b887a783b09d8b1077377fb291d4d4a6d4771746
eylon gilad <elon.gilad9@gmail.com>
2023-12-19 01:51:39 +0200

    added testering and update fanction, all work now

--- mobylie/src/research/imgDetector.py
+++ mobylie/src/research/imgDetector.py
@@ -4,98 +4,110 @@ from mobylie.src.research.obj import General_Object as obj
 
 class imgDetector:
     def __init__(self):
-        self.carArray=[]
-        self.signArray=[]
-        self.ourCar=obj.General_Object(Bounding_Box.Bounding_Box(0,0,0,0),"our_car")
-    def updateCar(self,res):
-        cars=[]
-        objSplited=res.split("\n")
+        self.carArray = []
+        self.signArray = []
+        self.ourCar = obj.General_Object(Bounding_Box.Bounding_Box(0, 0, 0, 0), "our_car")
+
+    def updateCar(self, res):
+        cars = []
+        objSplited = res.split("\n")
         for objText in objSplited:
-            objText=objText.replace("[","")
-            objText=objText.replace("]","")
-            info=objText.split(",")
-            if(len(info)==5):
-                box=Bounding_Box.Bounding_Box(int(float(info[0])),
-                                              int(float(info[1])),
-                                              int(float(info[2])),
-                                              int(float(info[3])))#create bounding box
-                                                    #name
-                cars.append(obj.General_Object(box,info[4]))#add to list
-        if(len(self.carArray)>0):
-            self.compareList(cars,True)
+            objText = objText.replace("[", "")
+            objText = objText.replace("]", "")
+            info = objText.split(",")
+            if len(info) == 5:
+                box = Bounding_Box.Bounding_Box(int(float(info[0])),
+                                                int(float(info[1])),
+                                                int(float(info[2])),
+                                                int(float(info[3])))  # create bounding box
+                # name
+                cars.append(obj.General_Object(box, info[4]))  # add to list
+        if (len(self.carArray) > 0):
+            self.compareList(cars, True)
         else:
             self.copyCarArr(cars)
-    def updateSign(self,res):
-        signs=[]
-        objSplited=res.split("\n")
+
+    def updateSign(self, res):
+        signs = []
+        objSplited = res.split("\n")
         for objText in objSplited:
-            objText=objText.replace("[","")
-            objText=objText.replace("]","")
-            info=objText.split(",")
-            if(len(info)==5):
-               box=Bounding_Box.Bounding_Box(int(float(info[0])),
-                                              int(float(info[1])),
-                                              int(float(info[2])),
-                                              int(float(info[3])))#create bounding box                                     #name
-               signs.append(obj.General_Object(box,info[4]))#add to list
-        if(len(self.signArray)>0):
-            self.compareList(signs,False)
+            objText = objText.replace("[", "")
+            objText = objText.replace("]", "")
+            info = objText.split(",")
+            if (len(info) == 5):
+                box = Bounding_Box.Bounding_Box(int(float(info[0])),
+                                                int(float(info[1])),
+                                                int(float(info[2])),
+                                                int(float(info[
+                                                              3])))  # create bounding box                                     #name
+                signs.append(obj.General_Object(box, info[4]))  # add to list
+        if len(self.signArray) > 0:
+            self.compareList(signs, False)
         else:
             self.copySignArr(signs)
 
     def updateOurCar(self):
-        sum = obj.Data(obj.Vec(0,0),obj.Vec(0,0),obj.Vec(0,0))
+        sum = obj.Data(obj.Vec(0, 0), obj.Vec(0, 0), obj.Vec(0, 0))
         posNonZeroCounter = 0
         velNonZeroCounter = 0
         accNonZeroCounter = 0
-        #adds them up to have the avg
-        for sign in self.signArray:
-            if sign.data.position.x != 0 and sign.data.position.y != 0:
-                sum.position+=sign.data.position
-                posNonZeroCounter+=1
-            if sign.data.velocity.x != 0 and sign.data.velocity.y != 0:
-                sum.velocity+=sign.data.velocity
-                velNonZeroCounter+=1
-            if sign.data.aceloration.x != 0 and sign.data.aceloration.y != 0:
-                sum.aceloration+=sign.data.aceloration
-                posNonZeroCounter+=1
+        # adds them up to have the avg
+        for sign in self.carArray:
+            if sign.distanceBetweenTwoFrames.x != 0 or sign.distanceBetweenTwoFrames.y != 0:
+                sum.position.x += sign.distanceBetweenTwoFrames.x
+                sum.position.y += sign.distanceBetweenTwoFrames.y
+                posNonZeroCounter += 1
+            if sign.data.velocity.x != 0 or sign.data.velocity.y != 0:
+                sum.velocity.x += sign.data.velocity.x
+                sum.velocity.y += sign.data.velocity.y
+                velNonZeroCounter += 1
+            if sign.data.aceloration.x != 0 or sign.data.aceloration.y != 0:
+                sum.aceloration.x += sign.data.aceloration.x
+                sum.aceloration.y += sign.data.aceloration.y
+                accNonZeroCounter += 1
         try:
-            #calc avg
-            sum.position.x=sum.position.x/posNonZeroCounter
-            sum.position.y=sum.position.y/posNonZeroCounter
-            self.ourCar.data.position+=sum.position
-            self.ourCar.data.velocity.x=sum.velocity.x/velNonZeroCounter
-            self.ourCar.data.velocity.y=sum.velocity.y/velNonZeroCounter
-            self.ourCar.data.aceloration.x=sum.aceloration.x/accNonZeroCounter
-            self.ourCar.data.aceloration.y=sum.aceloration.y/accNonZeroCounter
+            # calc avg
+            if posNonZeroCounter != 0:
+                sum.position.x = sum.position.x / posNonZeroCounter
+                sum.position.y = sum.position.y / posNonZeroCounter
+                self.ourCar.data.position.x += sum.position.x
+                self.ourCar.data.position.y += sum.position.y
+            if velNonZeroCounter != 0:
+                self.ourCar.data.velocity.x = sum.velocity.x / velNonZeroCounter
+                self.ourCar.data.velocity.y = sum.velocity.y / velNonZeroCounter
+            if accNonZeroCounter != 0:
+                self.ourCar.data.aceloration.x = sum.aceloration.x / accNonZeroCounter
+                self.ourCar.data.aceloration.y = sum.aceloration.y / accNonZeroCounter
         except ZeroDivisionError:
-            #if there wasnt enough information to calc it
-            print("zero info")
+            print("devided by zero")
 
-    def compareList(self,temp,carVector):
-        IUO_THRESHOLD=0.5
+    def compareList(self, temp, carVector):
+        IUO_THRESHOLD = 0.6
         for tempObj in temp:
             if carVector:
                 for car in self.carArray:
-                    iou=tempObj.bounding_box.calculateIUO(car.bounding_box)
-                    if car.name==tempObj.name and iou>=IUO_THRESHOLD:
-                        #tempObj.update()
+                    iou = tempObj.bounding_box.calculateIUO(car.bounding_box)
+                    if car.name == tempObj.name and iou >= IUO_THRESHOLD:
+                        tempObj.update(car)
+                        print(tempObj.id)
                         break
             else:
                 for sign in self.signArray:
-                    iou=tempObj.bounding_box.calculateIUO(sign.bounding_box)
-                    if sign.name==tempObj.name and iou>=IUO_THRESHOLD:
-                        #tempObj.update(sign)
+                    iou = tempObj.bounding_box.calculateIUO(sign.bounding_box)
+                    if sign.name == tempObj.name and iou >= IUO_THRESHOLD:
+                        tempObj.update(sign)
                         break
-            if carVector:
-                self.copyCarArr(temp)
-            else:
-                self.copySignArr(temp)
-    def copyCarArr(self,temp):
+        if carVector:
+            self.copyCarArr(temp)
+        else:
+            self.copySignArr(temp)
+
+    def copyCarArr(self, temp):
         self.carArray.clear()
         for car in temp:
             self.carArray.append(car)
-    def copySignArr(self,temp):
+
+    def copySignArr(self, temp):
         self.signArray.clear()
         for sign in temp:
             self.carArray.append(sign)
--- mobylie/src/research/main.py
+++ mobylie/src/research/main.py
@@ -5,45 +5,54 @@ import cv2
 import videoLoader
 import time
 
+
 def main():
-    loader=videoLoader.VideoLoader("../videos/highway1.mp4")
-    dettector=imgDetector.imgDetector()
-    while(loader.nextFrame()):
-        start_time=time.time()
-        carRes=detectCar(loader._img._bgrImg)
-        signRes=detectSign((loader._img._bgrImg))
+    loader = videoLoader.VideoLoader("../videos/highway1.mp4")
+    dettector = imgDetector.imgDetector()
+    while loader.nextFrame():
+        start_time = time.time()
+        carRes = detectCar(loader._img._bgrImg)
+        signRes = detectSign((loader._img._bgrImg))
         dettector.updateCar(carRes)
         dettector.updateSign(signRes)
         dettector.updateOurCar()
         ################################test###################################
         for car in dettector.carArray:
+            top_left = car.bounding_box.getTopLeftPoint(car.bounding_box.x, car.bounding_box.y, car.bounding_box.width,
+                                                        car.bounding_box.length)
+            bottom_right = car.bounding_box.getBottomRightPoint(car.bounding_box.x, car.bounding_box.y,
+                                                                car.bounding_box.width, car.bounding_box.length)
+            intTopLeft = (int(top_left[0]), int(top_left[1]))  # parse from float to int
+            intBottomRight = (int(bottom_right[0]), int(bottom_right[1]))  # parse from float to int
+            cv2.rectangle(loader._img._bgrImg, intTopLeft, intBottomRight, (255, 0, 0), 1)
 
-            top_left=car.bounding_box.getTopLeftPoint(car.bounding_box.x,car.bounding_box.y,car.bounding_box.width,car.bounding_box.length)
-            bottom_right=car.bounding_box.getBottomRightPoint(car.bounding_box.x,car.bounding_box.y,car.bounding_box.width,car.bounding_box.length)
-            intTopLeft=(int(top_left[0]),int(top_left[1]))#parse from float to int
-            intBottomRight=(int(bottom_right[0]),int(bottom_right[1]))#parse from float to int
-            cv2.rectangle(loader._img._bgrImg,intTopLeft,intBottomRight,(255,0,0),1)
-
-            text_position = (intTopLeft[0], intBottomRight[1]-10)
+            text = car.name + str(car.id)
+            text_position = (intTopLeft[0], intBottomRight[1] - 10)
             font_face = cv2.FONT_HERSHEY_SIMPLEX
             font_scale = 0.5
             text_color = (255, 0, 0)
             text_thickness = 1
-            cv2.putText(loader._img._bgrImg, car.name,
+            cv2.putText(loader._img._bgrImg, text,
                         text_position, font_face,
                         font_scale, text_color, text_thickness)
         ################################test###################################3
 
+        ########################test############################
+        print("-------------------------our car-------------------------------")
+        print((dettector.ourCar.data.position.x, dettector.ourCar.data.position.y))
+        print((dettector.ourCar.data.velocity.x, dettector.ourCar.data.velocity.y))
+        print((dettector.ourCar.data.aceloration.x, dettector.ourCar.data.aceloration.y))
+        ########################test############################
 
-        end_time=time.time()
+        end_time = time.time()
         print("time:")
-        print(end_time-start_time)
+        print(end_time - start_time)
 
-
-
-        cv2.imshow("bgr",loader._img._bgrImg)
-        cv2.imshow("hsl",loader._img._hlsImg)
+        cv2.imshow("bgr", loader._img._bgrImg)
+        cv2.imshow("hsl", loader._img._hlsImg)
         if cv2.waitKey(1) == ord('q'):
             break
+
+
 if __name__ == '__main__':
     main()
